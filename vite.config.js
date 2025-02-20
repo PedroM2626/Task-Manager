8a25c64eed515c649082c1457f95e8007234a648
PedroM2626 <pedromoratolahoz@gmail.com>
2025-02-20 18:26:43 -0300

    Deploy fix 2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Permite que a aplicação escute em todas as interfaces
-    port: Number(process.env.PORT) || 10000 // Usa a variável de ambiente PORT ou 10000 por padrão
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 10000
+  },
+  preview: {
+    allowedHosts: ["task-manager-16h0.onrender.com"]
   }
 })
